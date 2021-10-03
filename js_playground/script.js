09b499e7a72bb7fa34b9d15fd390b96cfe9a159d
isabelvonah <isabelvonah@gmail.com>
2021-10-03 18:35:02 +0200

    realizing the Todo-List Exercise (201/202)

--- js_playground/script.js
+++ js_playground/script.js
@@ -1,1 +1,34 @@
-console.log("Hello from my first file!")
+let openApp = true;
+
+let todos = [];
+
+let userInput = prompt('What would you like to do?');
+
+while (userInput != 'quit' && userInput != 'q') {
+
+	if (userInput == 'new') {
+		let newTodo = prompt('Insert new Todo.');
+		todos.push(newTodo);	
+	}
+
+	else if (userInput == 'list'){
+		console.log('******************')
+		for (let i = 0; i < todos.length; i++){
+			console.log(`${i}: ${todos[i]}`);
+		}
+		console.log('******************')
+	}
+
+	else if (userInput == 'delete'){
+		let deleteIndex = prompt('Which Todo do you want to delete? (index)');
+		todos.splice(deleteIndex);
+	}
+
+	//else if (userInput == 'quit'){break}
+	//<-- This solution is valid, but it is easier to include this into the while condition instead...
+
+	else {alert('Use one of the four options, please.')}
+
+	userInput = prompt('What would you like to do?');
+
+}
